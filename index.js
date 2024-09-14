6455b2d0e0b5fdbd740c9d44e0d95983cf58bc66
jagdishbutte <jagdishbutte@gmail.com>
2024-09-14 12:27:30 +0530

    added frontend source code

--- index.js
+++ index.js
@@ -7,19 +7,37 @@ const users = [];
 
 app.use(express.json());
 
+function auth(req, res, next){
+    const token = req.headers.token;
+    const decodeToken = jwt.verify(token, JWT_SECRET);
+
+    if(decodeToken.username){
+        req.username = decodeToken.username;
+        next();
+    }
+    else{
+        res.json({
+            message: "Something went wrong..."
+        });
+    }
+}
+
+
 app.post("/signup", (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
     users.push({
         username,
-        password
+        password,
+        tasks: []
     });
     
-    console.log({
+    res.json({
         message: "You are signed up successfully!"
     });
 });
+
 
 app.post("/signin", (req, res) =>{
     const username = req.body.username;
@@ -36,11 +54,69 @@ app.post("/signin", (req, res) =>{
         const token = jwt.sign({
             username: foundUser.username
         }, JWT_SECRET);
+
+        res.send({
+            token: token
+        });
     }
     else{
-        console.log({
+        res.json({
             message: "You have entered wrong credentials..."
         });
     }
+});
+
+
+
+app.get("/home", auth, (req,res)=>{
     
+    let foundUser = null;
+
+    for(let i = 0; i<users.length; i++){
+        if(users[i].username === req.username){
+            foundUser = users[i];
+        }
+    }
+
+    if(!foundUser){
+        res.status(401).json({
+            message: "Please login first..."
+        });
+    }
+
+    res.json({
+        username: foundUser.username,
+        password: foundUser.password
+    });
+
+});
+
+
+app.post("/addtask", auth, (req, res)=>{
+    const task = req.body.task;
+
+    let foundUser = null;
+    for(i = 0; i<users.length; i++){
+        if(users[i].username === username && users[i].password === password){
+            foundUser = users[i];
+        }
+    }
+
+    if (!foundUser) {
+        return res.status(401).json({
+            message: "Please login first..."
+        });
+    }
+
+    foundUser.tasks.push(task);
+
+    res.json({
+        message: "Task added successfully", 
+        tasks: foundUser.tasks
+    });
+});
+
+
+app.listen(3000, ()=>{
+    console.log("App is listening on port 3000");
 });
---
+++ public/script.js
@@ -0,0 +1,38 @@
+async function signup() {
+    const username = document.getElementById("signup-un").value;
+    const password = document.getElementById("signup-pass").value;
+
+    const response = await axios.post("http://localhost:4000/signup", {
+        username: username,
+        password: password
+    });
+    alert("You are signed up")
+}
+
+async function signin() {
+    const username = document.getElementById("signin-un").value;
+    const password = document.getElementById("signin-pass").value;
+
+    const response = await axios.post("http://localhost:4000/signin", {
+        username: username,
+        password: password
+    });
+
+    localStorage.setItem("token", response.data.token)
+
+    alert("You are signed in");
+}
+
+async function addTask() {
+    const response = await axios.get("http://localhost:4000/home", {
+        headers: {
+            token: localStorage.getItem("token")
+        }
+    });
+    document.getElementById("information").innerHTML = "Your Tasks: " + response.data.tasks
+}
+addTask();
+
+function logout() {
+    localStorage.removeItem("token");
+}
